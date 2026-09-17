3d50513978a7db06b970e3e1d28ed8cb700e52b2
henghaiyangfan-cmd <henghaiyangfan@gmail.com>
2026-09-18 03:49:35 +0800

    Refresh PWA cache after relay fix

--- web/public/sw.js
+++ web/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "bitchat-pwa-v6";
+const CACHE_NAME = "bitchat-pwa-v7";
 const BASE_PATH = new URL(self.registration.scope).pathname.replace(/\/$/, "");
 const APP_SHELL = [
   `${BASE_PATH}/`,
